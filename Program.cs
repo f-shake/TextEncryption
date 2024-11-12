eae6dda31f44df391b0c1a57455b52260f64011b
autodotua <autodotua@outlook.com>
2024-11-12 14:57:11 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using System;
+using TextEncryption;
+using System.Diagnostics;
+
+
+start:
+Console.Clear();
+Console.WriteLine("==========文本加密工具==========");
+string? password = null;
+while (string.IsNullOrWhiteSpace(password))
+{
+    Console.Write("请输入密码：");
+    password = Console.ReadLine()?.Trim();
+}
+
+int type = 0;
+while (type is not (1 or 2))
+{
+    Console.Write("加密（1）/解密（2）：");
+    char key = Console.ReadKey().KeyChar;
+    if (key is '1' or '2')
+    {
+        type = key - '0';
+    }
+    Console.WriteLine();
+}
+
+string? inputText = null;
+while (string.IsNullOrWhiteSpace(inputText))
+{
+    Console.WriteLine("请输入文本或UTF8格式的文件地址：");
+    inputText = Console.ReadLine()?.Trim();
+}
+if (File.Exists(inputText))
+{
+    inputText = File.ReadAllText(inputText);
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+TextEncryptor te = new TextEncryptor(password);
+if (type == 1)
+{
+    string encryptedText = te.Encrypt(inputText);
+    string test = te.Decrypt(encryptedText);
+    if (test != inputText)
+    {
+        throw new Exception("测试解密后的文本与源文本不同，需要检查算法");
+    }
+    Console.WriteLine("==========密文==========");
+    Console.WriteLine(encryptedText);
+    Console.WriteLine("==========密文==========");
+}
+else
+{
+    string decryptedText = te.Decrypt(inputText);
+    Console.WriteLine("==========原文==========");
+    Console.WriteLine(decryptedText);
+    Console.WriteLine("==========原文==========");
+}
+
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("按R以重新开始");
+
+if (Console.ReadKey().Key == ConsoleKey.R)
+{
+    goto start;
+}
---
+++ TextEncryptor.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextEncryption;
+public class TextEncryptor
+{
+    private readonly static UnicodeRange[] rangeLookup;
+
+    private readonly string password;
+
+    private readonly Dictionary<UnicodeRange, char[]> reversedShuffledCharLists = new Dictionary<UnicodeRange, char[]>();
+
+    private readonly Dictionary<UnicodeRange, char[]> shuffledCharLists = new Dictionary<UnicodeRange, char[]>();
+
+    static TextEncryptor()
+    {
+        rangeLookup = CreateRangeLookup();
+    }
+    public TextEncryptor(string password)
+    {
+        this.password = password;
+    }
+
+    public string Decrypt(string input)
+    {
+        var output = new StringBuilder(input.Length);
+        int passwordIndex = 0;
+
+        foreach (char ch in input)
+        {
+            if (NeedProcess(ch))
+            {
+                UnicodeRange range = rangeLookup[ch];
+                GetShuffledCharLists(range, out _, out char[] charList);
+                char newChar = charList[ch - range.Start];
+
+                output.Append(newChar);
+                passwordIndex++;
+            }
+            else
+            {
+                output.Append(ch); // Keep characters not in any range as is
+            }
+        }
+
+        return output.ToString();
+    }
+
+    public string Encrypt(string input)
+    {
+        var output = new StringBuilder(input.Length);
+        int passwordIndex = 0;
+
+        foreach (char ch in input)
+        {
+            if (NeedProcess(ch))
+            {
+                UnicodeRange range = rangeLookup[ch];
+                GetShuffledCharLists(range, out char[] charList, out _);
+                char newChar = charList[ch - range.Start];
+
+                output.Append(newChar);
+                passwordIndex++;
+            }
+            else
+            {
+                output.Append(ch);
+            }
+        }
+
+        return output.ToString();
+    }
+
+    private static UnicodeRange[] CreateRangeLookup()
+    {
+        var lookup = new UnicodeRange[char.MaxValue + 1];
+        foreach (var range in UnicodeRange.Ranges)
+        {
+            for (int i = range.Start; i <= range.End; i++)
+            {
+                lookup[i] = range;
+            }
+        }
+        return lookup;
+    }
+
+    private void GenerateShuffleList(UnicodeRange range)
+    {
+        var charList = new char[range.End - range.Start + 1];
+        for (int i = 0; i < charList.Length; i++)
+        {
+            charList[i] = (char)(i + range.Start);
+        }
+
+        ShuffleList(charList, password);
+        var reversedCharList = new char[range.End - range.Start + 1];
+        for (int i = 0; i < reversedCharList.Length; i++)
+        {
+            reversedCharList[charList[i] - range.Start] = (char)(i + range.Start);
+        }
+        shuffledCharLists[range] = charList;
+        reversedShuffledCharLists[range] = reversedCharList;
+    }
+
+    private void GetShuffledCharLists(UnicodeRange range,
+            out char[] shuffleList,
+        out char[] reversedShuffleList)
+    {
+        if (!shuffledCharLists.ContainsKey(range))
+        {
+            GenerateShuffleList(range);
+        }
+        shuffleList = shuffledCharLists[range];
+        reversedShuffleList = reversedShuffledCharLists[range];
+    }
+    private bool NeedProcess(char ch) => !char.IsControl(ch)
+                        && !char.IsWhiteSpace(ch)
+                        && !char.IsPunctuation(ch)
+                        && rangeLookup[ch] != null;
+
+    private void ShuffleList(char[] list, string password)
+    {
+        long seed = 0;
+        for (int i = 0; i < password.Length; i++)
+        {
+            seed = (seed * 31 + password[i]) % int.MaxValue;
+        }
+        MersenneTwister mt = new MersenneTwister((uint)seed);
+
+        for (int i = list.Length - 1; i > 0; i--)
+        {
+            int j = mt.Next(0, i + 1);
+            (list[i], list[j]) = (list[j], list[i]);
+        }
+    }
+
+}
+
